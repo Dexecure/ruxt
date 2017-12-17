1960fd76b795b7c96d8f185b5e235f6acb1b65db
Inian Parameshwaran <inian1234@gmail.com>
2017-12-17 12:51:37 +0800

    GA id used only in production

--- env-config.js
+++ env-config.js
@@ -2,4 +2,5 @@ const prod = process.env.NODE_ENV === "production";
 
 module.exports = {
   "process.env.BACKEND_URL": prod ? "https://ruxt-api.dexecure.com" : "https://ruxt-api.dexecure.com",
+  "process.env.GA_ID": prod ? "UA-55423842-6" : "UA-xxxxxxxx-x",
 };
