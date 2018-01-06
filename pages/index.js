fa4a44cf211ca9fc40b4fc2eac25f5329ff809d6
Hadeel Khawaldeh <hadylgk87@gmail.com>
2018-01-06 12:00:31 +0200

    add-formatted-link-to-Github (#16)

--- pages/index.js
+++ pages/index.js
@@ -35,6 +35,7 @@ class RUXtestApp extends React.Component {
     return (
       <div>
         <Meta />
+        <a href="https://github.com/Dexecure/ruxt"><img className="gitHub-img" src="https://camo.githubusercontent.com/82b228a3648bf44fc1163ef44c62fcc60081495e/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f6c6566745f7265645f6161303030302e706e67" alt="Fork me on GitHub" data-canonical-src="https://s3.amazonaws.com/github/ribbons/forkme_left_red_aa0000.png" /></a>
         <header className="dashboard--header">
           <div className="l">
             <div className="header--left">
