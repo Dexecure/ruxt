467a8cc9898c2797fed43ce14ca3725c2575bf8a
MacBook Pro <hadylgk87@gmail.com>
2018-01-10 21:15:48 +0200

    extract Explanation component

--- components/result.js
+++ components/result.js
@@ -8,7 +8,7 @@ import Slider from "react-rangeslider";
 import { debounce } from "underscore";
 import { PulseLoader } from "react-spinners";
 import Visual from "../components/visual";
-import Human from "../components/human";
+import Explanation from "./explanation";
 
 const humanCount = 1000;
 const defaultUrl = "https://google.com";
@@ -379,62 +379,7 @@ class ResultComponent extends React.Component {
               </span>
             </div>
           </div>
-          <div className="explanation__wrapper">
-            <div className="explanation__row">
-              <div className="explanation__item">
-                <div className="explanation__header">
-                  <span className="explanation__text">
-                    How to use the tool
-                  </span>
-                </div>
-                <div className="explanation__section">
-                  <span className="explanation__text">
-                    - Select a website using the autocomplete.<br />
-                    - (Optional) select a device and connection type. <br />
-                    - Use the time slider to select the user wait time.
-                  </span>
-                </div>
-              </div>
-              <div className="explanation__item">
-                <div className="explanation__header">
-                  <span className="explanation__text">
-                    Assume 1000 website visitors
-                  </span>
-                </div>
-                <div className="explanation__section">
-                  <span className="explanation__text">
-                    - <Human color="#ffffff" /> : no content loaded,<br />
-                    - <Human color="#5486AA" /> : some content loaded,<br />
-                    - <Human color="#153B58" /> : document loaded.
-                  </span>
-                </div>
-              </div>
-            </div>
-            <div className="explanation__header">
-              <span className="explanation__text">
-                Metrics
-              </span>
-            </div>
-            <div className="explanation__section">
-              <span className="explanation__text">
-                - <a href="https://dexecure.com/blog/chrome-user-experience-report-explained-google-bigquery/#diving-into-the-important-questions-wheee">Site Experience Benchmark (SEB)</a> score: the fraction of users completing first contentful paint within first second.<br />
-                - The percentage of users completing <a href="https://developers.google.com/web/updates/2017/06/user-centric-performance-metrics#first_paint_and_first_contentful_paint">first contentful paint</a> within given time.<br />
-                - The percentage of users completing document load within given time.<br />
-              </span>
-            </div>
-            <div className="explanation__header">
-              <span className="explanation__text">
-                Learn more
-              </span>
-            </div>
-            <div className="explanation__section">
-              <span className="explanation__text">
-                - Read more on CrUX and the metrics for user experience in <a href="https://dexecure.com/blog/chrome-user-experience-report-explained-google-bigquery/">the introductory article on CrUX</a>.<br />
-                - Contribute at <a href="https://github.com/dexecure/ruxt">GitHub</a>. Suggestions welcome.<br />
-                - Reach out at <a href="mailto:coffee@dexecure.com">coffee@dexecure.com</a>.
-              </span>
-            </div>
-          </div>
+          <Explanation />
         </div>
       </div>
     );
--- pages/compare.js
+++ pages/compare.js
@@ -4,7 +4,7 @@ import Router from "next/router";
 import Select from "react-select";
 import Slider from "react-rangeslider";
 import Meta from "../components/meta";
-import Human from "../components/human";
+import Explanation from "../components/explanation";
 import ResultGraph from "../components/resultGraph";
 
 const devAndconDefault = "all";
@@ -175,62 +175,7 @@ class CompareComponent extends React.Component {
               time={this.state.time}
             />
           </div>
-          <div className="explanation__wrapper">
-            <div className="explanation__row">
-              <div className="explanation__item">
-                <div className="explanation__header">
-                  <span className="explanation__text">
-                    How to use the tool
-                  </span>
-                </div>
-                <div className="explanation__section">
-                  <span className="explanation__text">
-                    - Select a website using the autocomplete.<br />
-                    - (Optional) select a device and connection type. <br />
-                    - Use the time slider to select the user wait time.
-                  </span>
-                </div>
-              </div>
-              <div className="explanation__item">
-                <div className="explanation__header">
-                  <span className="explanation__text">
-                    Assume 1000 website visitors
-                  </span>
-                </div>
-                <div className="explanation__section">
-                  <span className="explanation__text">
-                    - <Human color="#ffffff" /> : no content loaded,<br />
-                    - <Human color="#5486AA" /> : some content loaded,<br />
-                    - <Human color="#153B58" /> : document loaded.
-                  </span>
-                </div>
-              </div>
-            </div>
-            <div className="explanation__header">
-              <span className="explanation__text">
-                Metrics
-              </span>
-            </div>
-            <div className="explanation__section">
-              <span className="explanation__text">
-                - <a href="https://dexecure.com/blog/chrome-user-experience-report-explained-google-bigquery/#diving-into-the-important-questions-wheee">Site Experience Benchmark (SEB)</a> score: the fraction of users completing first contentful paint within first second.<br />
-                - The percentage of users completing <a href="https://developers.google.com/web/updates/2017/06/user-centric-performance-metrics#first_paint_and_first_contentful_paint">first contentful paint</a> within given time.<br />
-                - The percentage of users completing document load within given time.<br />
-              </span>
-            </div>
-            <div className="explanation__header">
-              <span className="explanation__text">
-                Learn more
-              </span>
-            </div>
-            <div className="explanation__section">
-              <span className="explanation__text">
-                - Read more on CrUX and the metrics for user experience in <a href="https://dexecure.com/blog/chrome-user-experience-report-explained-google-bigquery/">the introductory article on CrUX</a>.<br />
-                - Contribute at <a href="https://github.com/dexecure/ruxt">GitHub</a>. Suggestions welcome.<br />
-                - Reach out at <a href="mailto:coffee@dexecure.com">coffee@dexecure.com</a>.
-              </span>
-            </div>
-          </div>
+          <Explanation />
         </div>
         <style jsx>{`
           .URLCompare__wrapper {
